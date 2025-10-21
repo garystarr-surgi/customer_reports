422aadffc2827901b435fe130b2ca0fe5ffdf6c3
garystarr-surgi <gary.starr@surgishop.com>
2025-10-21 08:51:19 -0400

    Update hooks.py

--- customer_reports/hooks.py
+++ customer_reports/hooks.py
@@ -1,5 +1,4 @@
 app_version = "0.0.1"
-
 app_name = "customer_reports"
 app_title = "Customer Reports"
 app_publisher = "SurgiShop"
@@ -9,20 +8,60 @@ app_license = "MIT"
 
 # Includes in fixtures
 fixtures = [
-    "Custom Field",
-    "Client Script",
-    "Report"
+    {
+        "dt": "Report",
+        "filters": [
+            ["name", "in", ["Customer Item Purchase History"]]
+        ]
+    },
+    {
+        "dt": "Custom Field",
+        "filters": [
+            ["dt", "=", "Customer"],
+            ["fieldname", "like", "customer_reports%"]
+        ]
+    },
+    {
+        "dt": "Client Script",
+        "filters": [
+            ["dt", "=", "Customer"],
+            ["name", "like", "%customer_reports%"]
+        ]
+    }
 ]
 
-# Desktop module registration
-# app_include_js = "/assets/customer_reports/js/customer_item_purchase_history.js"
+# Include JS files
+# app_include_js = "/assets/customer_reports/js/customer_reports.bundle.js"
+
+# Include CSS files
+# app_include_css = "/assets/customer_reports/css/customer_reports.css"
 
 # Optional: register your module in the desktop
 app_icon = "octicon octicon-graph"
 app_color = "#589494"
-app_module = "Customer Reports"
+
+# Module registration
+# This makes your app appear in the modules list
+app_include_js = [
+    "/assets/customer_reports/js/customer_reports.min.js"
+]
+
+# Doctype JS (optional - if you want to override Customer form)
+# doctype_js = {
+#     "Customer": "public/js/customer.js"
+# }
+
+# Boot session info (optional)
+# boot_session = "customer_reports.boot.boot_session"
+
+# Scheduled Tasks (optional)
+# scheduler_events = {
+#     "daily": [
+#         "customer_reports.tasks.daily"
+#     ]
+# }
 
 # Optional: if you use patches
 # patches = [
-#     "customer_reports.patches.patch_customer_report_setup"
+#     "customer_reports.patches.v0_0.setup_customer_reports"
 # ]
