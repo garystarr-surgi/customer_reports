371303c113d9eeca0660ed2bdb33d723d784090b
garystarr-surgi <gary.starr@surgishop.com>
2025-10-21 12:26:15 -0400

    Update client script to match Frappe Cloud version exactly

--- customer_reports/client_script/customer.js
+++ customer_reports/client_script/customer.js
@@ -1,20 +1,12 @@
 frappe.ui.form.on('Customer', {
-  refresh(frm) {
-    if (frm.doc.__islocal) return;
-
-    const html_field = frm.fields_dict['custom_purchase_report'];
-    if (html_field) {
-      html_field.$wrapper.html(`
-        <button class="btn btn-primary btn-sm" id="view-purchase-report">
-          View Purchase History Report
-        </button>
-      `);
-
-      html_field.$wrapper.find('#view-purchase-report').on('click', () => {
-        frappe.set_route('query-report', 'Customer Item Purchase History', {
-          customer: frm.doc.name
-        });
-      });
+    refresh: function(frm) {
+        if (!frm.is_new()) {
+            // Add a custom button in the "Reports" group
+            frm.add_custom_button(__('Purchase History'), function() {
+                frappe.set_route('query-report', 'Customer Item Purchase History', {
+                    customer: frm.doc.name
+                });
+            }, __('Reports'));
+        }
     }
-  }
 });
