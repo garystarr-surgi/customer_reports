d8be9a051b10341f6d4d860576e9fd6e88aacfe5
garystarr-surgi <gary.starr@surgishop.com>
2025-10-21 10:13:20 -0400

    Update customer_item_purchase_history.js

--- customer_reports/assets/js/customer_item_purchase_history.js
+++ customer_reports/assets/js/customer_item_purchase_history.js
@@ -1,1 +1,29 @@
-
+frappe.query_reports["Customer Item Purchase History"] = {
+    "filters": [
+        {
+            "fieldname": "customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer",
+            "reqd": 1
+        },
+        {
+            "fieldname": "item_code",
+            "label": __("Item"),
+            "fieldtype": "Link",
+            "options": "Item"
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -12)
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        }
+    ]
+};
